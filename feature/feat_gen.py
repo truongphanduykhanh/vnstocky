d35bfb9854fc1b5e6043289279c5bea76ff05af9
khanhtpd <khanhtpd@techcombank.com.vn>
2021-08-17 23:09:03 +0700

    refactor gen feat

--- feature/feat_gen.py
+++ feature/feat_gen.py
@@ -3,10 +3,11 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
 
-bs = pd.read_csv('bs_data.csv', dtype={'Ticker': str, 'Feat_Time': str})
+income = pd.read_csv('income_data.csv', dtype={'Ticker': str, 'Feat_Time': str})
+balance = pd.read_csv('balance_data.csv', dtype={'Ticker': str, 'Feat_Time': str})
 
 
-def get_common_bs(
+def get_common_fs(
     bs,
     remove_common=[
         'Non_Current_Assets', 'Tangible_Assets', 'Leased_Assets', 'Intangible_Assets',
@@ -26,41 +27,62 @@ def get_common_bs(
     return output_df
 
 
-def get_last_dates(input_time,
-                   delta=1,
-                   input_format='%Y%m%d',
-                   output_format='%Y%m%d'
-                   ):
-    '''
-    Get list of last dates of months or weeks
-
-    Parameters
-    ----------
-    input_time : str
-        input time. Ex: '20210630'
-    delta : int
-        number of month wanted to look future. Ex: 1
-    input_format : str
-        format of input time. Ex: '%Y%m' or '%Y%m%d'
-    output_format : str
-        format of output time. Ex: '%Y%m' or '%Y%m%d'
-
-    Returns
-    -------
-    list of str
-        last date of next (delta) month. Ex: '20210731'
-    '''
-    input_time = datetime.datetime.strptime(input_time, input_format)
-    output_time = input_time + relativedelta(months=delta)
-    output_time = output_time.replace(day=28) + relativedelta(days=4)
-    output_time = output_time - relativedelta(days=output_time.day)
-    output_time = output_time.strftime(output_format)
-    return output_time
+def calculate_roll_mean(df, window):
+    meta = df[['Ticker', 'Feat_Time']]
+    roll_mean = (
+        df
+        .groupby('Ticker')
+        .rolling(window)
+        .mean()
+        .shift(-window + 1)
+        .reset_index(drop=True)
+    )
+    roll_mean = roll_mean.add_suffix(f'_Mean_{window}Q')
+    roll_mean = pd.concat([meta, roll_mean], axis=1)
+    return roll_mean
 
 
-bs_feat = (
-    get_common_bs(bs)
-    .assign(Feat_Time=lambda df: df['Feat_Time'].apply(get_last_dates, delta=1))
-)
+def shift_data(df, periods):
+    meta = df[['Ticker', 'Feat_Time']]
+    shift = (
+        df
+        .drop('Feat_Time', axis=1)
+        .groupby('Ticker')
+        .shift(-periods)
+        .reset_index(drop=True)
+    )
+    shift = pd.concat([meta, shift], axis=1)
+    return shift
 
-bs_feat.to_csv('bs_feat.csv', index=False)
+
+def calculate_momentum(df, window, periods):
+    meta = df[['Ticker', 'Feat_Time']]
+
+    roll_mean = calculate_roll_mean(df, window)
+    shift = shift_data(roll_mean, periods)
+
+    roll_mean = roll_mean.drop(['Ticker', 'Feat_Time'], axis=1)
+    shift = shift.drop(['Ticker', 'Feat_Time'], axis=1)
+
+    momentum = roll_mean.div(shift)
+    momentum = momentum.add_suffix(f'_Momen_{periods}Q')
+    momentum = pd.concat([meta, momentum], axis=1)
+    return momentum
+
+
+def calculate_roll_mean_momentum(df, window_list=[1, 2, 4], periods_list=[1, 2, 4]):
+    meta = df[['Ticker', 'Feat_Time']]
+    roll_mean_momentum = []
+    for window in window_list:
+        for periods in periods_list:
+            momentum_window_periods = calculate_momentum(df, window, periods)
+            momentum_window_periods = momentum_window_periods.drop(['Ticker', 'Feat_Time'], axis=1)
+            roll_mean_momentum.append(momentum_window_periods)
+    roll_mean_momentum = pd.concat([meta] + roll_mean_momentum, axis=1)
+    return roll_mean_momentum
+
+
+income_momen = calculate_roll_mean_momentum(income)
+balance_momen = calculate_roll_mean_momentum(balance)
+income_common = get_common_fs(income)
+balance_common = get_common_fs(balance)
